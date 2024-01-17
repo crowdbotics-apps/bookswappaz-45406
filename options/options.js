64e9320ff924e5cb6df19ff14815d1b8b2f6a572
Crowdbotics <team@crowdbotics.com>
2024-01-17 17:33:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bookswappaz-45406",
+  url: "https://bookswappaz-45406.botics.co",
+  api: "https://bookswappaz-45406.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
